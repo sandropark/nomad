e820adda72a834f66ed8029ff1824924ccf466b7
sandropark <ticoba@naver.com>
2024-08-05 22:31:13 +0900

    [연습용 앱] 버튼 클릭 시 글씨 바뀌는 기능 추가

--- prac_app/app/src/main/java/com/sandro/prac_app/MainActivity.kt
+++ prac_app/app/src/main/java/com/sandro/prac_app/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.sandro.prac_app
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val tv: TextView = findViewById(R.id.tv_hello)
+        val btn: Button = findViewById(R.id.btn_kr)
+
+        btn.setOnClickListener {
+            if (btn.text == "한글로") {
+                tv.text = "안녕"
+                btn.text = "영어로"
+            }
+
+            if (btn.text == "영어로") {
+                tv.text = "Hello"
+                btn.text = "한글로"
+            }
         }
     }
 }
