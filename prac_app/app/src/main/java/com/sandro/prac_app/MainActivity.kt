bd8d9f3112a0be2f34d13cc50e41c1848d7f28c6
sandropark <ticoba@naver.com>
2024-08-07 23:24:04 +0900

    [연습용 앱] 임포트 수정

--- prac_app/app/src/main/java/com/sandro/prac_app/MainActivity.kt
+++ prac_app/app/src/main/java/com/sandro/prac_app/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.example.prac_app.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
