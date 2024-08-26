303f343132b5f94cf1b95c235f697328d65e7b96
sandropark <ticoba@naver.com>
2024-08-26 20:44:29 +0900

    [메모리 게임] 이미지 추가 뷰 생성

--- MemoryGame/app/src/main/java/com/sandro/memorygame/CreateActivity.kt
+++ MemoryGame/app/src/main/java/com/sandro/memorygame/CreateActivity.kt
@@ -1,17 +1,28 @@
 package com.sandro.memorygame
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.sandro.memorygame.models.BoardSize
 import com.sandro.memorygame.utils.EXTRA_BOARD_SIZE
 
 class CreateActivity : AppCompatActivity() {
+    private lateinit var rvImagePicker: RecyclerView
+    private lateinit var btnSave: Button
+    private lateinit var etGameName: EditText
+
     private lateinit var boardSize: BoardSize
+
     private var numImageRequired = -1
+    private val chosenImageUris = mutableListOf<Uri>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +34,10 @@ class CreateActivity : AppCompatActivity() {
             insets
         }
 
+        rvImagePicker = findViewById(R.id.rvImagePicker)
+        btnSave = findViewById(R.id.btnSave)
+        etGameName = findViewById(R.id.etGameName)
+
         // 뒤로가기 버튼 활성화
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         // intent로 넘어온 boardSize 꺼내기
@@ -30,6 +45,10 @@ class CreateActivity : AppCompatActivity() {
         numImageRequired = boardSize.getNumPairs()
         // 액션바 제목 설정
         supportActionBar?.title = "Choose pics (0 / ${numImageRequired})"
+
+        rvImagePicker.adapter = ImagePickerAdapter(this, chosenImageUris, boardSize)
+        rvImagePicker.setHasFixedSize(true)
+        rvImagePicker.layoutManager = GridLayoutManager(this, boardSize.getWidth())
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
